248eae6c025d42d057584c54df1cf43529547427
Luis Felipe Nicolau <luisfnicolau@hotmail.com>
2018-08-26 16:02:50 -0400

    Create third screen

--- run_in/lib/introduction/third_page.dart
+++ run_in/lib/introduction/third_page.dart
@@ -1,6 +1,10 @@
-import 'package:flutter/material.dart';
+import 'dart:ui';
 
-class ThirdPage extends StatelessWidget{
+import 'package:flutter/material.dart';
+import 'package:run_in/tools/myDivider.dart';
+import 'package:run_in/tools/roundedOutlineButtom.dart';
+
+class ThirdPage extends StatelessWidget {
   TabController _tabController;
 
   ThirdPage(TabController tabController) {
@@ -9,11 +13,56 @@ class ThirdPage extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-      return Column(
+    return Material(
+      child: Column(
         children: <Widget>[
-          Container()
+          Container(
+            height: 64.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Mas antes...',
+                style: TextStyle(fontSize: 24.0),
+              ),
+            ),
+          ),
+          myDivider(),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Queremos saber um pouco sobre você, os diga mais ou menos seu nível de treino.',
+                style: TextStyle(fontSize: 16.0),
+              ),
+            ),
+          ),
+          myDivider(),
+          myRoundedOutlineButtom(
+            text: 'Nunca corro',
+              onPressed: _onButtonPressed
+          ),
+          myRoundedOutlineButtom(
+              text: 'Uma vez por semana',
+              onPressed: _onButtonPressed
+          ),
+          myRoundedOutlineButtom(
+              text: 'Três vezes por semana',
+              onPressed: _onButtonPressed
+          ),
+          myRoundedOutlineButtom(
+              text: 'Todo dia',
+              onPressed: _onButtonPressed
+          )
         ],
-      );
+      ),
+    );
   }
 
+  void _onButtonPressed() {
+    print('OLAAAA');
+  }
 }
---
+++ run_in/lib/tools/roundedOutlineButtom.dart
@@ -0,0 +1,42 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class myRoundedOutlineButtom extends StatelessWidget {
+
+  final String text;
+  final Function onPressed;
+
+  const myRoundedOutlineButtom({
+    Key key,
+    @required this.text,
+    @required this.onPressed,
+  })  : assert(text != null),
+        assert(onPressed != null),
+        super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 32.0, bottom: 8.0),
+      child: Center(
+        child: Container(
+          width: 200.0,
+          height: 50.0,
+          child: new OutlineButton(
+            borderSide: BorderSide(color: Colors.white),
+            color: Colors.transparent,
+            onPressed: onPressed,
+            shape: new RoundedRectangleBorder(
+              borderRadius: new BorderRadius.circular(24.0),
+            ),
+            child: BackdropFilter(
+                filter: new ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+                child: Text(text)),
+          ),
+        ),
+      ),
+    );
+  }
+}
